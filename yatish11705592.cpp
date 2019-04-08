8ad7579a85eef88a38e615fd89fc11d64ebcd282
yatishsharma31 <43512336+yatishsharma31@users.noreply.github.com>
2019-04-08 22:37:25 +0530

    Update yatish11705592.cpp

--- yatish11705592.cpp
+++ yatish11705592.cpp
@@ -1,21 +1,36 @@
 #include<stdio.h>
-#include<process.h>
+#include<stdlib.h>
 int main()
-{  
+{
 	int p[20],at[20],bt[20],tat[20],tbt=0,n,total=0,pos,temp=0;
-	float priority[20],avg_wt,avg_tat;
-	
+	float avg_wt=0,avg_tat,z,k,l;
 	printf("\nEnter the number of processes:");
 	scanf("%d",&n);
-    
 	for(int i=0;i<n;i++)
 	{
-		printf("\nEnter the arrival time for p%d:",i);
+		printf("\nFor Process[%d]\n\n",i+1);
+		printf("Arrival Time:");
+		while(1){
 		scanf("%d",&at[i]);
-		printf("\nEnter the burst time for p%d:",i);
+		if(at[i]>=0){
+		    break;}
+		else
+		{   
+			printf("plz enter valid Arrival time\t");
+		}
+	}
+		printf("Burst time:");
+		while(1){
 		scanf("%d",&bt[i]);
+		if(bt[i]>0){
+		    break;}
+		else
+		{
+			printf("plz enter valid burst time\t");
+		}
+	}
 		tbt+=bt[i];
-		p[i]=i;   //having process numbers
+		p[i]=i+1;   //having process numbers
 	}
 	printf("total burst time%d\n",tbt);
 	for(int i=0;i<n;i++)
@@ -34,44 +49,52 @@ int main()
 		 bt[j+1]=temp;
 	   }
 	}
-}
-system("cls");
-system("color 5b");
-printf("processes\t\tArival time\t\tBurst time\n");
-for(int i=0;i<n;i++)
-{
-	printf("p[%d]\t\t\t%d\t\t%d\n",p[i],at[i],bt[i]);
-}
-int wt[n];
-wt[0]=0;
-for(int i=1;i<n;i++)
-{
-wt[i]=at[i-1]+bt[i-1]+wt[i-1]-at[i];
-if(wt[i]<0)
-{
-wt[i]=0;
-}
-}
-
-for(int i=0;i<n;i++){
-	printf("waiting times for p(%d)\t\t %d\n",p[i],wt[i]);
-}
-int x[n];
-int count=1,z=0;
-for(int i=0;i<n;i++)
-{
-	if(at[i]==at[i+1] &&i<n-1)
+  }
+  int max=-1,count=0,wt[n],wf[n],pf[n];
+  int nat[n],nbt[n];
+  wf[0]=0;
+  pf[0]=p[0];
+  avg_tat=tbt;
+  tbt=tbt-bt[0];
+  system("cls");
+  system("color 4b");
+ while(tbt>0)
+ { temp=0;
+ count++;
+ wt[0]=0;
+ for(int i =count;i<n;i++)
+ { wt[i]=at[count-1]+bt[count-1]+wf[count-1]-at[i];
+	if(wt[i]<0)
 	{
-		count++;
+	wt[i]=0;
+		}
+    k=wt[i];
+    l=bt[i];
+    z=k/l;
+	if(z>max)	
+    {   wf[count]=wt[i];
+    	max=z;
+    	pos=i;
 	}
-	else
-	{
-	x[z]=count;
-	printf("common values %d and position strart from %d to %d\n",x[z],(i-x[z]+1),i);
-	count=1;
-	z++;
-    }
+ }
+ max=-1;
+ pf[count]=p[pos];
+ temp=p[count];
+ p[count]=p[pos];
+ p[pos]=temp;
+ temp=at[count];
+ at[count]=at[pos];
+ at[pos]=temp;
+ temp=bt[count];
+ bt[count]=bt[pos];
+ bt[pos]=temp;
+ tbt=tbt-bt[count];
+ avg_wt=avg_wt+wf[count];
+  }
+  avg_tat=avg_tat+avg_wt;
+  for(int i=0;i<n;i++)
+  {
+  	printf("process(%d) \n\twaiting time %d \n\t Turn Around time is %d\n",pf[i],wf[i],wf[i]+bt[i]);
+  }
+  printf("Average  Waiting time %0.3f and Turn Around time %0.3f\n",avg_wt/n,avg_tat/n);
 }
-	
-}
-
